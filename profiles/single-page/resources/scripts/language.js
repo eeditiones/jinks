acef10da494a5c891e50c9d18824025cee51dc7b
Wolfgang Meier <wolfgang@existsolutions.com>
2024-06-25 18:28:58 +0000

    single-page: refactor to extend plain profile

--- profiles/single-page/resources/scripts/language.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    pbEvents.subscribe('pb-i18n-language', null, (ev) => {
-        const { language } = ev.detail;
-        window.location.href = `?lang=${language}`;
-    });
-});
