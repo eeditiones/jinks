c84c711415116665b92d54a6209bc88647f26536
Wolfgang Meier <wolfgang@existsolutions.com>
2024-07-18 06:26:06 +0000

    Port the print preview to base10

--- profiles/base10/resources/scripts/dark.js
+++ profiles/base10/resources/scripts/dark.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const modeSwitch = document.getElementById('colorMode');
-    modeSwitch.addEventListener('click', function() {
-        let theme = document.body.dataset.theme || localStorage.getItem('tp.theme');
-        if (!theme && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            // User prefers dark mode
-            theme = 'dark';
-        }
-        const newTheme = theme === 'dark' ? 'light' : 'dark';
-        document.body.dataset.theme = newTheme;
-        localStorage.setItem('tp.theme', newTheme);
-    });
+    if (modeSwitch) {
+        modeSwitch.addEventListener('click', function() {
+            let theme = document.body.dataset.theme || localStorage.getItem('tp.theme');
+            if (!theme && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                // User prefers dark mode
+                theme = 'dark';
+            }
+            const newTheme = theme === 'dark' ? 'light' : 'dark';
+            document.body.dataset.theme = newTheme;
+            localStorage.setItem('tp.theme', newTheme);
+        });
+    }
 
     let theme = document.body.dataset.theme || localStorage.getItem('tp.theme');
     if (theme) {
