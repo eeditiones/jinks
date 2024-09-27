00575d40c2d4ce69a23c22220681f6b77e23787f
Wolfgang Meier <wolfgang@existsolutions.com>
2024-09-27 09:29:27 +0000

    correspondence: add correspondence profile with timeline

---
+++ profiles/correspondence/modules/correspondence-api.xql
@@ -0,0 +1,28 @@
+xquery version "3.1";
+
+module namespace corresp="http://teipublisher.com/api/correspondence";
+
+import module namespace config="http://www.tei-c.org/tei-simple/config" at "config.xqm";
+
+declare function corresp:timeline($request as map(*)) {
+    let $entries := session:get-attribute($config:session-prefix || '.hits')
+    let $datedEntries := filter($entries, function($entry) {
+            try {
+                let $date := ft:field($entry, "date", "xs:date")
+                return
+                    exists($date) and year-from-date($date) != 1000
+            } catch * {
+                false()
+            }
+        })
+    return
+        map:merge(
+            for $entry in $datedEntries
+            group by $date := ft:field($entry, "date", "xs:date")
+            return
+                map:entry(format-date($date, "[Y0001]-[M01]-[D01]"), map {
+                    "count": count($entry),
+                    "info": ''
+                })
+        )
+};
---
+++ profiles/correspondence/resources/scripts/correspondence.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const facets = document.getElementById('timeline-options');
+    const timelineChanged = (ev) => {
+        let categories = ev.detail.categories;
+        if (ev.detail.scope === '5Y') {
+            expandDates(categories, 5);
+        } else if (ev.detail.scope === '10Y') {
+            expandDates(categories, 10);
+        }
+        document.querySelectorAll('[name=dates]').forEach(input => { input.value = categories.join(';') });
+        facets.submit();
+    };
+    pbEvents.subscribe('pb-timeline-date-changed', 'timeline', timelineChanged);
+    pbEvents.subscribe('pb-timeline-daterange-changed', 'timeline', timelineChanged);
+    pbEvents.subscribe('pb-timeline-reset-selection', 'timeline', () => {
+        document.querySelectorAll('[name=dates]').forEach(input => { input.value = '' });
+        facets.submit();
+    });
+});
+
+function expandDates(categories, n) {
+    categories.forEach((category) => {
+        const year = parseInt(category);
+        for (let i = 1; i < n; i++) {
+            categories.push(year + i);
+        }
+    });
+}
