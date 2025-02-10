754d205d10919eac59f12628ca907226ef822fdd
Wolfgang Meier <wolfgangmm@gmail.com>
2025-02-10 17:43:09 +0000

    Use icon for copy incoming text button

--- resources/scripts/editor.js
+++ resources/scripts/editor.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function createOpenButtonHtml(abbrev) {
         return `<a id="open-action" class="action" href="../${abbrev}" target="_new">
-        <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512"><path d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
         </a>`;
     }
 
@@ -226,8 +226,9 @@ window.addEventListener('DOMContentLoaded', () => {
                     <span class='badge ${message.type === 'conflict' ? 'alert' : ''}'>${message.type}</span> 
                     ${message.path} ${message.source ? ' from ' + message.source : ''}`;
                     if (message.type === 'conflict' && message.incoming) {
-                        const copyButton = document.createElement('button');
-                        copyButton.innerText = 'Copy';
+                        const copyButton = document.createElement('a');
+                        copyButton.href = '#';
+                        copyButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512"><rect x="128" y="128" width="336" height="336" rx="57" ry="57" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path d="M383.5 128l.5-24a56.16 56.16 0 00-56-56H112a64.19 64.19 0 00-64 64v216a56.16 56.16 0 0056 56h24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>`;
                         copyButton.addEventListener('click', (ev) => {
                             ev.preventDefault();
                             navigator.clipboard.writeText(message.incoming).then(() => {
