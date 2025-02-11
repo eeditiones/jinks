4c3b307e9b5d5a4755a6d4f2d852d8edd817120b
Wolfgang Meier <wolfgangmm@gmail.com>
2025-02-11 09:56:55 +0000

    Add tooltips for copy and compare buttons

--- resources/scripts/editor.js
+++ resources/scripts/editor.js
@@ -228,6 +228,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     if (message.type === 'conflict' && message.incoming) {
                         const copyButton = document.createElement('a');
                         copyButton.href = '#';
+                        copyButton.dataset.tooltip = 'Copy incoming version to clipboard';
                         copyButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512"><rect x="128" y="128" width="336" height="336" rx="57" ry="57" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path d="M383.5 128l.5-24a56.16 56.16 0 00-56-56H112a64.19 64.19 0 00-64 64v216a56.16 56.16 0 0056 56h24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>`;
                         copyButton.addEventListener('click', (ev) => {
                             ev.preventDefault();
@@ -241,6 +242,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
                         const cmpButton = document.createElement('a');
                         cmpButton.href = '#';
+                        cmpButton.dataset.tooltip = 'Compare current version to incoming';
                         cmpButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M304 160l-64-64 64-64M207 352l64 64-64 64"/><circle cx="112" cy="96" r="48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><circle cx="400" cy="416" r="48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M256 96h84a60 60 0 0160 60v212M255 416h-84a60 60 0 01-60-60V144" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>`;
                         li.appendChild(cmpButton);
                         cmpButton.addEventListener('click', (ev) => {
