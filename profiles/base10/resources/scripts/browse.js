c38295912b8ef08abbb22f463729825f3484161c
wolfgangmm <wolfgangmm@exist-db.org>
2025-07-09 07:39:49 +0000

    Button styling

--- profiles/base10/resources/scripts/browse.js
+++ profiles/base10/resources/scripts/browse.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    /* Scan for links to collections and handle clicks */
+    function handleCollectionLinks(root) {
+        root.querySelectorAll('[data-collection]').forEach((link) => {
+            link.addEventListener('click', (ev) => {
+                ev.preventDefault();
+
+                const collection = link.getAttribute('data-collection');
+                // write the collection into a hidden input and resubmit the search
+                document.querySelector('.options [name=collection]').value = collection;
+                pbEvents.emit('pb-search-resubmit', 'search');
+            });
+        });
+    }
+
     let loginCount = 0;
     pbEvents.subscribe('pb-login', null, function(ev) {
         if (ev.detail.userChanged && loginCount > 0) {
@@ -6,6 +21,8 @@ document.addEventListener('DOMContentLoaded', function () {
             ++loginCount;
         }
     });
+
+    handleCollectionLinks(document);
 
     /* Parse the content received from the server */
     pbEvents.subscribe('pb-results-received', 'search', function(ev) {
@@ -26,17 +43,13 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelectorAll('[can-write]').forEach((elem) => {
             elem.disabled = !writable;
         });
-
-        /* Scan for links to collections and handle clicks */
-        content.querySelectorAll('[data-collection]').forEach((link) => {
-            link.addEventListener('click', (ev) => {
-                ev.preventDefault();
-
-                const collection = link.getAttribute('data-collection');
-                // write the collection into a hidden input and resubmit the search
-                document.querySelector('.options [name=collection]').value = collection;
-                pbEvents.emit('pb-search-resubmit', 'search');
-            });
+        document.querySelectorAll('.parent-link').forEach((link) => {
+            const parentCollection = currentCollection.replace(/^(.*?)\/?[^/]*$/, "$1");
+            link.setAttribute('data-collection', parentCollection);
         });
+        document.querySelectorAll('.current-collection').forEach((elem) => {
+            elem.textContent = currentCollection;
+        });
+        handleCollectionLinks(content);
     });
 });
