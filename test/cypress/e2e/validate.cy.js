9925e3ab1f143a33a3b108814b2a3e4188a609cc
duncdrum <d.paterson@me.com>
2025-07-25 16:11:44 +0200

    fix(api.json): bump openapi version
    include openapi schema
    
    see OAI/OpenAPI-Specification#4149

--- test/cypress/e2e/validate.cy.js
+++ test/cypress/e2e/validate.cy.js
@@ -15,6 +15,12 @@ describe('Dynamic JSON Schema Validation', () => {
   const jinksApi = 'modules/api.json'
   const openApiSchema = 'schema/openapi-3.0.json'
 
+  before(() => {
+    cy.readFile(openApiSchema).then(schema => {
+      ajv4.addSchema(schema, 'https://spec.openapis.org/oas/3.0/schema/2021-09-28')
+    })
+  })
+
   it('validates jinks.json against the JSON Schema draft-07 meta-schema', () => {
     cy.readFile(jinksSchema).then(jinksSchema => {
       const metaSchema = ajv7.getSchema('http://json-schema.org/draft-07/schema').schema
@@ -34,23 +40,19 @@ describe('Dynamic JSON Schema Validation', () => {
     })
   })
 
-  it('validates jinks api.json against the JSON Schema https://spec.openapis.org/oas/3.0/schema/2021-09-28', () => {
-    cy.readFile(openApiSchema).then(schema => {
-      ajv4.addSchema(schema, 'https://spec.openapis.org/oas/3.0/schema/2021-09-28')
-      cy.readFile(jinksApi).then(data => {
-        cy.validateJsonSchema(ajv4, schema, data, jinksApi)
-      })
+  it('validates jinks api.json against OpenAPI 3.0.3 schema', () => {
+    cy.readFile(jinksApi).then(data => {
+      const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
+      cy.validateJsonSchema(ajv4, schema, data, jinksApi)
     })
   })
 
-  it.skip('validates all api.json files against OpenAPI 3.0 schema', () => {
-    cy.findFiles('**/api.json').then(apiFiles => {
-      cy.readFile(openApiSchema).then(schema => {
-        ajv4.addSchema(schema, 'https://spec.openapis.org/oas/3.0/schema/2021-09-28')
-        apiFiles.forEach(apiPath => {
-          cy.readFile(apiPath).then(data => {
-            cy.validateJsonSchema(ajv4, schema, data, apiPath)
-          })
+  it('validates all api.tpl.json files against OpenAPI 3.0.3 schema', () => {
+    const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
+    cy.findFiles('**/*api.tpl.json').then(apiFiles => {
+      apiFiles.forEach(apiPath => {
+        cy.readFile(apiPath).then(data => {
+          cy.validateJsonSchema(ajv4, schema, data, apiPath)
         })
       })
     })
