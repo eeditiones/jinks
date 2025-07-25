8e15fb1d6a77e8d3227c0f5464e539422f10a13e
duncdrum <d.paterson@me.com>
2025-07-25 17:52:01 +0200

    fix(e2e): add scaffolds
    api docs and template tester
    
    see #70

--- test/cypress/e2e/validate.cy.js
+++ test/cypress/e2e/validate.cy.js
@@ -69,14 +69,14 @@ describe('Static JSON Schema Validation', () => {
 
   describe('openApi', () => {
 
-    it('jinks api.json should validate agains OpenAPI 3.0.3 schema', () => {
+    it('jinks api.json should validate against OpenAPI 3.0.3 schema', () => {
       cy.readFile(jinksApi).then(data => {
         const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
         cy.validateJsonSchema(ajv4, schema, data, jinksApi)
       })
     })
 
-    it('jinks deploy-api.json should validate agains OpenAPI 3.0.3 schema', () => {
+    it('jinks deploy-api.json should validate against OpenAPI 3.0.3 schema', () => {
       const jinksDeploy ='modules/deploy-api.json'
       cy.readFile(jinksDeploy).then(data => {
         const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
@@ -84,7 +84,7 @@ describe('Static JSON Schema Validation', () => {
       })
     })
 
-    it('profiles api.tpl.json should validate agains OpenAPI 3.0.3 schema', () => {
+    it('profiles api.tpl.json should validate against OpenAPI 3.0.3 schema', () => {
       const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
       cy.findFiles('**/*api.tpl.json').then(apiFiles => {
         apiFiles.forEach(apiPath => {
@@ -95,7 +95,8 @@ describe('Static JSON Schema Validation', () => {
       })
     })
 
-    it('custom-api.json (docs) should validate agains OpenAPI 3.0.3 schema', () => {
+    it('custom-api.json (docs) should validate against OpenAPI 3.0.3 schema', () => {
+      // TODO: For consistency this should probably be custom-api.tpl.json
       const customApi = 'profiles/docs/modules/custom-api.json'
       cy.readFile(customApi).then(data => {
         const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
