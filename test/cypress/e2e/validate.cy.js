d7808e0ce2061689434187552a7473dd919fcfe1
duncdrum <d.paterson@me.com>
2025-07-25 17:39:35 +0200

    fix(validate.cy.js): extend coverage
    see #73

--- test/cypress/e2e/validate.cy.js
+++ test/cypress/e2e/validate.cy.js
@@ -10,7 +10,7 @@ addFormats(ajv7)
 const ajv4 = new Ajv4({ allErrors: true, strict: false })
 addFormats(ajv4)
 
-describe('Dynamic JSON Schema Validation', () => {
+describe('Static JSON Schema Validation', () => {
   const jinksSchema = 'schema/jinks.json'
   const jinksApi = 'modules/api.json'
   const openApiSchema = 'schema/openapi-3.0.json'
@@ -34,52 +34,72 @@ describe('Dynamic JSON Schema Validation', () => {
       ajv7.addSchema(cleanSchema)
     })
   })
-
-  it('validates jinks.json against the JSON Schema draft-07 meta-schema', () => {
-    cy.readFile(jinksSchema).then(jinksSchema => {
-      const metaSchema = ajv7.getSchema('http://json-schema.org/draft-07/schema').schema
-      cy.validateJsonSchema(ajv7, metaSchema, jinksSchema, 'schema/jinks.json')
+  describe('Jinks', () => {
+    it('own schema should validate against the draft-07 meta-schema', () => {
+      cy.readFile(jinksSchema).then(jinksSchema => {
+        const metaSchema = ajv7.getSchema('http://json-schema.org/draft-07/schema').schema
+        cy.validateJsonSchema(ajv7, metaSchema, jinksSchema, 'schema/jinks.json')
+      })
     })
-  })
 
-  it('validates all config.json files against jinks.json', () => {
-    const schema = ajv7.getSchema('https://tei-publisher.org/jinks/config/schema')
-    if (!schema) {
-      throw new Error('Schema not found')
-    }
+    it('profiles config.json should validate against main schema', () => {
+      const schema = ajv7.getSchema('https://tei-publisher.org/jinks/config/schema')
+      if (!schema) {
+        throw new Error('Schema not found')
+      }
 
-    cy.findFiles('profiles/**/config.json').then(configFiles => {
-      configFiles.forEach(configPath => {
-        cy.readFile(configPath).then(data => {
-          // Pre-validation logging only
-          cy.log('Validating:', {
-            file: configPath,
-            required: {
-              id: { present: 'id' in data, value: data.id },
-              type: { present: 'type' in data, value: data.type },
-              label: { present: 'label' in data, value: data.label }
-            }
+      cy.findFiles('profiles/**/config.json').then(configFiles => {
+        configFiles.forEach(configPath => {
+          cy.readFile(configPath).then(data => {
+            // Pre-validation logging only
+            cy.log('Validating:', {
+              file: configPath,
+              required: {
+                id: { present: 'id' in data, value: data.id },
+                type: { present: 'type' in data, value: data.type },
+                label: { present: 'label' in data, value: data.label }
+              }
+            })
+            cy.validateJsonSchema(ajv7, schema.schema, data, configPath)
           })
-          cy.validateJsonSchema(ajv7, schema.schema, data, configPath)
         })
       })
     })
   })
 
-  it('validates jinks api.json against OpenAPI 3.0 schema', () => {
-    cy.readFile(jinksApi).then(data => {
-      const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
-      cy.validateJsonSchema(ajv4, schema, data, jinksApi)
-    })
-  })
+  describe('openApi', () => {
 
-  it('validates all api.tpl.json files against OpenAPI 3.0 schema', () => {
-    const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
-    cy.findFiles('**/*api.tpl.json').then(apiFiles => {
-      apiFiles.forEach(apiPath => {
-        cy.readFile(apiPath).then(data => {
-          cy.validateJsonSchema(ajv4, schema, data, apiPath)
+    it('jinks api.json should validate agains OpenAPI 3.0.3 schema', () => {
+      cy.readFile(jinksApi).then(data => {
+        const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
+        cy.validateJsonSchema(ajv4, schema, data, jinksApi)
+      })
+    })
+
+    it('jinks deploy-api.json should validate agains OpenAPI 3.0.3 schema', () => {
+      const jinksDeploy ='modules/deploy-api.json'
+      cy.readFile(jinksDeploy).then(data => {
+        const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
+        cy.validateJsonSchema(ajv4, schema, data, jinksDeploy)
+      })
+    })
+
+    it('profiles api.tpl.json should validate agains OpenAPI 3.0.3 schema', () => {
+      const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
+      cy.findFiles('**/*api.tpl.json').then(apiFiles => {
+        apiFiles.forEach(apiPath => {
+          cy.readFile(apiPath).then(data => {
+            cy.validateJsonSchema(ajv4, schema, data, apiPath)
+          })
         })
+      })
+    })
+
+    it('custom-api.json (docs) should validate agains OpenAPI 3.0.3 schema', () => {
+      const customApi = 'profiles/docs/modules/custom-api.json'
+      cy.readFile(customApi).then(data => {
+        const schema = ajv4.getSchema('https://spec.openapis.org/oas/3.0/schema/2021-09-28').schema
+        cy.validateJsonSchema(ajv4, schema, data, customApi)
       })
     })
   })
