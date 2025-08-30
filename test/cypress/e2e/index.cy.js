ac1c7fb9ed2334fa37e44a9d8107fa5dad406e31
duncdrum <d.paterson@me.com>
2025-08-30 13:12:41 +0200

    test(index): improve robustness
    see #106

--- test/cypress/e2e/index.cy.js
+++ test/cypress/e2e/index.cy.js
@@ -58,7 +58,7 @@ describe('index page', () => {
   })
 
   describe('content area', () => {
-    it('should 4 sections and 2 headers', () => {
+    it('should have 4 sections with 2 headers', () => {
       cy.get('h3')
         .should('have.length', 2)
         .contains('Configuration')
@@ -66,46 +66,7 @@ describe('index page', () => {
         .should('have.length', 4)
     })
 
-    // TP Base + Dark mode + docker
-    // see #106
-    it.skip('should simulate creating a custom app with selected features (dry mode)', () => {
-      cy.get('[name="abbrev"]')
-        .type('e2edry{enter}')
-      cy.get('[name="label"]')
-        .type(' application')
-      cy.get('[name="id"]')
-      cy.contains('https://e-editiones.org/apps/e2edry')
-      cy.get('[type="checkbox"]')
-        .check('docker')
-      cy.get('#appConfig')
-        .contains('docker')
-      cy.get('#dry-run')
-        .click()
-      cy.get('.error')
-        .should('not.exist')
-    })
-
-    it('should create a custom app with selected features (forced overwrite)', () => {
-      cy.get('[name="abbrev"]')
-        .type('e2etest{enter}')
-      cy.get('[name="label"]')
-        .type(' application')
-      cy.get('[name="id"]')
-      cy.contains('https://e-editiones.org/apps/e2etest')
-      cy.get('[type="checkbox"]')
-        .check('docker')
-      cy.get('#appConfig')
-        .contains('docker')
-      cy.get('[name="overwrite"]')
-        .select('all')
-      cy.get('#apply-config')
-        .click()
-      cy.contains('Package is deployed. Visit it here', { timeout: 50000 })
-    })
-
-
-
-    it('the profiles section has 3 subsections with sensible defaults', () => {
+    it('the profiles section should have 3 subsections selected defaults', () => {
       cy.get('#profiles > fieldset')
         .should('have.length.gte', 3)
       cy.get('#profiles')
@@ -123,7 +84,103 @@ describe('index page', () => {
         .should('have.length.gte', 15)
       cy.get(':checked')
         .should('have.length.gte', 2)
+      cy.get('.error')
+        .should('not.be.visible')
+    })
 
+    it('the custom ODD picker should modify config', () => {
+      cy.get('[name="custom-odd"]')
+        .type('e2etest.odd')
+      cy.get('#add-odd')
+        .click()
+      cy.get('#appConfig')
+        .contains('"odd": "e2etest.odd"')
+    })
+
+    it.skip('the toolbar should ...', () => {
+      cy.get('.toolbar')
+        .should('not.be.visible')
+      // TODO(DP)
+    })
+
+    // Missing test areas 
+    // generation with overwrite: force
+    // Actions panel 
+    // toolbar with generated app link icons (only visible with pre-existing apps)
+    // info pop ups and tooltips
+    // negative paths throughout the spec
+    // modify an existing app
+
+
+   
+  })
+
+  describe('deployments [side-effects]', () => {
+     // TP Base + Dark mode + docker
+    // see #106
+    it.skip('should simulate creating a custom app with selected features (dry mode)', () => {
+      cy.intercept({
+        'method': 'POST',
+        'pathname': '**/jinks/api/generator',
+        'query': {
+          overwrite: 'default',
+          dry: 'true',
+        }
+      })
+        .as('dryTest')
+
+      cy.get('[name="abbrev"]')
+        .type('e2edry{enter}')
+      cy.get('[name="label"]')
+        .type(' application')
+      cy.get('[name="id"]')
+      cy.contains('https://e-editiones.org/apps/e2edry')
+      cy.get('[type="checkbox"]')
+        .check('docker')
+      cy.get('#appConfig')
+        .contains('docker')
+      cy.get('#dry-run')
+        .click()
+      cy.wait('@dryTest')
+      cy.get('.error')
+        .contains('not.be.visible')
+    })
+
+    it('should force create a custom app with selected features [side-effects]', () => {
+      cy.intercept({
+        'method': 'POST',
+        'pathname': '**/jinks/api/generator',
+        'query': {
+          overwrite: 'all',
+        }
+      })
+        .as('e2eGenerate')
+
+      cy.intercept({
+        'method': 'POST',
+        'pathname': '**/api/generator/e2etest/deploy',
+      })
+        .as('e2eDeploy')
+
+      cy.get('[name="abbrev"]')
+        .type('e2etest{enter}')
+      cy.get('[name="label"]')
+        .type(' application')
+      cy.get('[name="id"]')
+      cy.contains('https://e-editiones.org/apps/e2etest')
+      cy.get('[type="checkbox"]')
+        .check('docker')
+      cy.get('#appConfig')
+        .contains('docker')
+      cy.get('[name="overwrite"]')
+        .select('all')
+      cy.get('#apply-config')
+        .click()
+      cy.wait('@e2eGenerate')
+      cy.wait('@e2eDeploy', { responseTimeout: 40000 })
+      cy.contains('Package is deployed. Visit it here', { timeout: 10000 })
+      cy.get('.error')
+        .should('not.be.visible')
     })
   })
 
