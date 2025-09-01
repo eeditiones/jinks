ec91b047c9c4d22a31b427e114dd5f998cba8fc3
duncdrum <d.paterson@me.com>
2025-09-01 12:59:04 +0200

    test(templating): playground tests
    close #70

--- test/cypress/e2e/templating.cy.js
+++ test/cypress/e2e/templating.cy.js
@@ -4,8 +4,42 @@ describe('Templating Tester', () => {
     cy.visit('templating.html')
   })
 
-  it('should show title header', () => {
+  it('should show editors and titles and cheatsheet', () => {
     cy.get('h1')
       .contains('Template')
+    cy.get('#template')
+      .should('be.visible')
+    cy.get('#parameters')
+      .should('be.visible')
+    cy.get('details')
+      .should('be.visible')
+    cy.get('[data-cy="cheatsheet-link"]')
+      .click()
+    cy.get('#cheatsheet')
+      .should('be.visible')
+      .find('tbody')
+      .contains('[%')
+  })
+
+  it('should allow interactive testing', () => {
+    cy.intercept({
+      'method': 'POST',
+      'pathname': '**/jinks/api/templates',
+    })
+      .as('templating')
+
+    cy.get('#examples')
+      .should('be.visible')
+      .select(1)
+    cy.get('#template')
+      .contains('[%')
+    cy.get('#eval')
+      .click()
+    cy.wait('@templating')
+    cy.get('errorMsg')
+      .should('not.exist')
+    cy.get('.output')
+      .should('be.visible')
+      .and('have.length.gte', 3)
   })
 })
